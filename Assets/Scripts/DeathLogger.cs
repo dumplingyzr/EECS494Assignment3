597afc92259882bce3d1a21251e78be542af0ae8
Ben Magee <benrm@umich.edu>
2014-04-14 14:18:51 -0400

    Actual jank as fuck death logger. goddam

--- Assets/Scripts/DeathLogger.cs
+++ Assets/Scripts/DeathLogger.cs
@@ -4,34 +4,50 @@ using System;
 
 public class DeathLogger : MonoBehaviour {
 	ArrayList log;
+	public GameObject g;
+	public GameObject level;
+	bool died;
+	DateTime tod;
+	public float duration;
 
 	// Use this for initialization
 	void Start () {
+		DontDestroyOnLoad (this);
 		log = new ArrayList ();
+		died = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		log.Add(LogInstance.Log (this.gameObject));
+		if (g) {
+			log.Add (new LogInstance (g));
+		} else if (!died) {
+			tod = DateTime.Now;
+			died = true;
+		}
 	}
-	
-	void OnDestroy () {
 
+	void OnDrawGizmos () {
+		foreach (LogInstance entry in log) {
+			if(!died || (tod - entry.timestamp).TotalSeconds < duration)
+				Gizmos.DrawRay (entry.position, entry.velocity);
+		}
 	}
 }
 
 class LogInstance {
-	Vector3 position;
-	Vector3 velocity;
-	DateTime timestamp;
+	public Vector3 position;
+	public Vector3 velocity;
+	public DateTime timestamp;
 
-	LogInstance (Vector3 pos, Vector3 vel) {
+	public LogInstance (Vector3 pos, Vector3 vel) {
 		position = pos;
-		velocity = vel;
-		DateTime timestamp = DateTime.Now;
+		velocity = vel * Time.deltaTime;
+		timestamp = DateTime.Now;
 		}
 
-	public static LogInstance Log (GameObject g) {
-		return new LogInstance (g.transform.position, g.rigidbody.velocity);
-		}
+	public LogInstance (GameObject g) :
+		this (g.transform.position, g.rigidbody.velocity)
+	{
+	}
 }

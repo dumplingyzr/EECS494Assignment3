a830096c8a237d4aa77f9c1cc02681371291a5ed
Ben Magee <benrm@umich.edu>
2014-04-14 13:38:34 -0400

    Jank as fuck death logger

---
+++ Assets/Scripts/DeathLogger.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class DeathLogger : MonoBehaviour {
+	ArrayList log;
+
+	// Use this for initialization
+	void Start () {
+		log = new ArrayList ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		log.Add(LogInstance.Log (this.gameObject));
+	}
+	
+	void OnDestroy () {
+
+	}
+}
+
+class LogInstance {
+	Vector3 position;
+	Vector3 velocity;
+	DateTime timestamp;
+
+	LogInstance (Vector3 pos, Vector3 vel) {
+		position = pos;
+		velocity = vel;
+		DateTime timestamp = DateTime.Now;
+		}
+
+	public static LogInstance Log (GameObject g) {
+		return new LogInstance (g.transform.position, g.rigidbody.velocity);
+		}
+}

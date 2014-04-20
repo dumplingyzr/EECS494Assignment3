b6e5159d9603733c991d7215bb854901d701eed5
Zhangrui Yao <yaozr@umich.edu>
2014-04-20 14:37:09 -0400

    changed rain effect. rain now indicates gravity change make followplayer and rain part of the stage, drag character into followplayer script and drag followplayer into rain script

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+	public Transform character;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = character.position;
+	}
+}
---
+++ Assets/Scripts/Rain.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rain : MonoBehaviour {
+	public Transform character;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		this.transform.localPosition = character.localPosition + new Vector3 (0, 300, 0);
+	}
+}

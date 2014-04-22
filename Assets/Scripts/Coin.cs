1c025036e12660e5433f0526bba0988b8bcfec52
Zhangrui Yao <yaozr@umich.edu>
2014-04-22 00:24:59 -0400

    added coin prefab and script

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("Spinning", 0, 0.01f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Spinning(){
+		transform.Rotate (0, 2, 0, Space.Self);
+	}
+}
